ee51a59dc9155109a1cac52d37edbe6f3e895901
MD Tajul Islam <mdtajulislammt505@gmail.com>
2024-03-21 23:26:27 +0600

    Update ContactUs.jsx

--- src/Pages/Home/ContactUs/ContactUs.jsx
+++ src/Pages/Home/ContactUs/ContactUs.jsx
@@ -7,7 +7,7 @@ const ContactUs = () => {
                     <img src={contactImg} alt="" className='w-full' />
                     <div className=' p-5 lg:pl-20 w-full'>
                          <h2 className=' text-4xl font-bold text-center'>Our Office</h2>
-                         <p className=' text-xl font-semibold text-center my-3'>1712-Tongi,Gazipur,Dhaka,Bangladesh </p>
+                         <p className=' text-xl font-semibold text-center my-3'>Tongi,Gazipur,Dhaka,Bangladesh </p>
                          <button className=' flex items-center gap-2 mx-auto bg-primary-yellow font-bold p-3 px-5 rounded'><FaLocationDot /> <span>Directions</span></button>
                     </div>
                </div>
